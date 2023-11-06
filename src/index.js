32aad4eb837c2d7b1f3e454d5ab687ea4ad33b19
Tejas <tejashinde4046@gmail.com>
2023-11-06 19:05:20 +0530

    Added controller and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import {PORT} from "./config/serverConfig.js";
 import {connect} from "./config/database.js";
+import bodyParser from "body-parser";
 const app=express();
 
 import TweetService from "./services/tweet-service.js";
+import ApiRoutes from './routes/index.js';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api',ApiRoutes);
 
 app.listen(PORT,async()=>{
     console.log('server started');
     
     await connect();
     console.log('Mongo db connected');
-    const service=new TweetService();
-    const tweet=await service.create({
-        content:'This is my #second tweet I joined the #twiter yesterday itself and i #Love it' 
-    });
-    console.log(tweet);
+    // const service=new TweetService();
+    // const tweet=await service.create({
+    //     content:'This is my #Third tweet ! my name is xyz from #Delhi' 
+    // });
+    // console.log(tweet);
 }); 
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -10,8 +10,8 @@ class TweetService{
         async create(data){
             const content=data.content;
             const tags=content.match(/#[a-zA-Z0-9_]+/g)
-                        .map((tag)=>tag.substring(1)).toLowerCase();
-
+                        .map((tag)=>tag.substring(1).toLowerCase());
+                        
             const tweet= await this.tweetRepository.create(data);
             let alreadyPresentTags=await this.hashtageRepository.findByName(tags);
             let PresentTagsTitle=alreadyPresentTags.map(tags => tags.title);
