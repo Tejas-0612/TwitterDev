5129b2d50c08f74c0d0942574b8869756643b8ed
Tejas <tejashinde4046@gmail.com>
2023-11-06 10:41:04 +0530

    added index file

--- src/index.js
+++ src/index.js
@@ -3,31 +3,10 @@ const app=express();
 const connect=require('./config/database');
 
 
-// const {HastagRepository}=require('./repository/index');
-
 app.listen(3000,async()=>{
     console.log('server started');
     
     await connect();
     console.log('Mongo db connected');
-    // const tagsrepo=new HastagRepository();
-    // const tags=await tagsrepo.create([
-    //     {
-    //         title:'Trend',
-    //         tweets:[]
-    //     },
-    //     {
-    //         title:'Love',
-    //         tweets:[]
-    //     },
-    //     {
-    //         title:'Cool',
-    //         tweets:[]
-    //     },
-    //     {
-    //         title:'MSD',
-    //         tweets:[]
-    //     },
-    // ]);
-    // console.log(tags);
+
 });
